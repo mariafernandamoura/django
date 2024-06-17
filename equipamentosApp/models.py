ea8bac8ed9b5cbd2f8797bfb93fdc9626c330be4
mariafernandamoura <cl202252@g.unicamp.br>
2024-06-17 00:23:33 -0300

    arrumar imagem

--- equipamentosApp/models.py
+++ equipamentosApp/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Equipamento(models.Model):
     id_equipamento = models.IntegerField
     local = models.CharField(max_length=100)
+    imagem = models.ImageField(upload_to='equipamentos/', null=True, blank=True)
 
     def __str__(self):
         return self.id
